188773a9c6e3fecff423ab8c46069c7bf791038b
OpportunV <Vadya.95@mail.ru>
2019-12-15 18:40:19 +0300

    day 15

--- d15.py
+++ d15.py
@@ -22,10 +22,44 @@ def get_full_map(intcode):
     size = 41
     field = np.zeros((size, size), dtype=int) - 1
     x, y = size // 2 + 1, size // 2 + 1
+    field[x, y] = 1
+    field[0, 0] = 0
+    field[0, -1] = 2
+    field[-1, -1] = 5
     
+    def right(move):
+        if move == 1:
+            return 3
+        if move == 2:
+            return 4
+        if move == 3:
+            return 2
+        if move == 4:
+            return 1
+    
+    def get_move():
+        last_move = 1
+        while True:
+            if reply == 0:
+                last_move = right(right(right(last_move)))
+                yield last_move
+            yield right(last_move)
+            if reply != 0:
+                last_move = right(last_move)
+                yield last_move
+            else:
+                yield last_move
+                
+    reply = 1
     done = False
+    # fig, ax = plt.subplots()
+    # img = plt.imshow(field)
+    # plt.ion()
+    # plt.axis(False)
+    # plt.show()
+    mover = get_move()
     while not done:
-        move = randint(1, 4)
+        move = next(mover)
         reply = comp.cor.send(move)
         next(comp.cor)
         prev_pos = x, y
@@ -41,17 +75,26 @@ def get_full_map(intcode):
         if reply == 0:
             x, y = prev_pos
         
+        # tmp =field[x, y]
+        # field[x, y] = 5
+        # img.set_data(field)
+        # plt.pause(.0000001)
+        # field[x, y] = tmp
+        
         xs, ys = np.where(field[1:-1, 1:-1] == -1)
-        for x, y in zip(xs + 1, ys + 1):
-            if not (field[x - 1, y] == 0
-                    and field[x + 1, y] == 0
-                    and field[x, y - 1] == 0
-                    and field[x, y + 1] == 0):
+        for _x, _y in zip(xs + 1, ys + 1):
+            if not (field[_x - 1, _y] == 0
+                    and field[_x + 1, _y] == 0
+                    and field[_x, _y - 1] == 0
+                    and field[_x, _y + 1] == 0):
                 break
         else:
+            field[0, 0] = -1
+            field[0, -1] = -1
+            field[-1, -1] = -1
             with open('d15_output.txt', 'w') as fout:
                 for line in field:
-                    print(' '.join([str(i) for i in line]), file=fout)
+                    print(' '.join([f'{i: d}' for i in line]), file=fout)
             return
 
 
@@ -125,6 +168,9 @@ def part_two(field):
         last_added = spread_air(last_added)
         time += 1
         img.set_data(field)
+        if 1 not in field:
+            print(time)
+            exit()
         return img
     
     time = 0
@@ -146,6 +192,6 @@ with open('d15_input.txt') as fin:
 # get_full_map(intc)
 field = np.loadtxt('d15_output.txt')
 
-print(part_one(field))
-# part_two(field)
+# print(part_one(field))
+part_two(field)
 
