0f562788868ae8aeb47d841e3da8c0d3c8480a59
OpportunV <Vadya.95@mail.ru>
2019-12-17 14:59:12 +0300

    day 17 added visualization

--- d17.py
+++ d17.py
@@ -31,15 +31,10 @@ def part_one(intcode):
         print(''.join(chr(i) for i in line))
     
     print(ans)
-    
-    from matplotlib import pyplot as plt
-    fig, ax = plt.subplots()
-    img = plt.imshow(field)
-    plt.axis(False)
-    plt.show()
+    return field
 
 
-def part_two(intcode):
+def part_two(intcode, field):
     intcode[0] = 2
     comp = IntcodeComputer(0, intcode)
     
@@ -48,7 +43,7 @@ def part_two(intcode):
         'L,6,6,L,6,L,8,R,6\n',
         'L,8,L,8,R,4,R,6,R,6\n',
         'L,6,6,R,6,L,8\n',
-        'n\n',
+        'y\n',
     ]
     
     print(chr(comp.first_val), end='', sep='')
@@ -63,18 +58,49 @@ def part_two(intcode):
             if repl:
                 print(chr(repl))
         
+        if pr == prog[-1]:
+            break
         while repl := next(comp.cor):
             if repl and chr(repl).isascii():
                 print(chr(repl), end='')
             else:
                 print(repl)
+                break
     
-    for i in range(10):
-        print(next(comp.cor))
+    def update(frame):
+        nonlocal frames, field
+        frames += 1
+        for i in range(len(field)):
+            for j in range(len(field[0])):
+                tmp = next(comp.cor)
+                while tmp == 10:
+                    tmp = next(comp.cor)
+                field[i, j] = tmp
+
+        img.set_data(field)
+        return img
+        
+    from matplotlib import pyplot as plt
+    from matplotlib.animation import FuncAnimation, FFMpegWriter
+    fig, ax = plt.subplots()
+    img = plt.imshow(field, interpolation='none')
+    plt.axis(False)
+    frames = 0
+    anim = FuncAnimation(fig, update, interval=0.1, frames=343)
+    writer = FFMpegWriter(fps=30)
+    plt.rcParams['animation.ffmpeg_path'] = r'D:\univ\Progin\ffmpeg\bin\ffmpeg.exe'
+    anim.save('d17.mp4', writer=writer)
 
 
 with open('d17_input.txt') as fin:
     intc = [int(i) for i in fin.readline().split(',')]
 
-# part_one(intc)
-part_two(intc)
+
+def main():
+    field = part_one(intc.copy())
+    part_two(intc, field)
+
+
+if __name__ == '__main__':
+    main()
+
