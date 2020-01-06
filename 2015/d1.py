d0488c53d45031010e63c97c810fbeba6ea36157
OpportunV <Vadya.95@mail.ru>
2020-01-06 17:31:23 +0300

    day 1 both

--- 2015/d1.py
+++ 2015/d1.py
@@ -1,14 +1,34 @@
-with open('d1_input.txt') as fin:
-    inp = fin.readline()
+def part_one(inp):
+    ans = 0
+    for i, char in enumerate(inp):
+        if char == '(':
+            ans += 1
+        else:
+            ans -= 1
     
-ans = 0
-for i, char in enumerate(inp):
-    if char == '(':
-        ans += 1
-    else:
-        ans -= 1
-    if ans == -1:
-        print(i + 1)
-        break
+    print(ans)
 
-print(ans)
+
+def part_two(inp):
+    ans = 0
+    for i, char in enumerate(inp):
+        if char == '(':
+            ans += 1
+        else:
+            ans -= 1
+        if ans == -1:
+            print(i + 1)
+            break
+
+
+def main():
+    with open('d1_input.txt') as fin:
+        inp = fin.readline()
+    
+    part_one(inp)
+    part_two(inp)
+
+
+if __name__ == '__main__':
+    main()
+
