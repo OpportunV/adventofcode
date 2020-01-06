70ab71d642aad7db38a84d5cc84361caa522fb52
OpportunV <Vadya.95@mail.ru>
2020-01-06 17:29:29 +0300

    day 1 both

--- 2015/d1.py
+++ 2015/d1.py
@@ -1,6 +1,14 @@
 with open('d1_input.txt') as fin:
     inp = fin.readline()
     
-print(inp)
+ans = 0
+for i, char in enumerate(inp):
+    if char == '(':
+        ans += 1
+    else:
+        ans -= 1
+    if ans == -1:
+        print(i + 1)
+        break
 
-
+print(ans)
