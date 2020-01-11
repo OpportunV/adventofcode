82c73d803c39196086c73932ae4baca09ae4efaa
OpportunV <Vadya.95@mail.ru>
2020-01-12 00:08:40 +0300

    day 7

--- 2015/d7.py
+++ 2015/d7.py
@@ -1,52 +1,21 @@
 def part_one(inp):
-    connections = {}
-    solved = {}
-    for line in inp:
-        if 'NOT' in line:
-            _, cur, _, tar = line.split()
-            connections[tar] = ['~', cur]
-        elif 'AND' in line:
-            cur1, _, cur2, _, tar = line.split()
-            connections[tar] = ['&', cur1, cur2]
-        elif 'OR' in line:
-            cur1, _, cur2, _, tar = line.split()
-            connections[tar] = ['|', cur1, cur2]
-        elif 'LSHIFT' in line:
-            cur1, _, cur2, _, tar = line.split()
-            connections[tar] = ['<<', cur1, cur2]
-        elif 'RSHIFT' in line:
-            cur1, _, cur2, _, tar = line.split()
-            connections[tar] = ['>>', cur1, cur2]
-        else:
-            cur, _, tar = line.split()
-            if cur.isdigit():
-                solved[tar] = int(cur)
-            else:
-                connections[tar] = [cur]
-    
-    def find(target):
-        if target in solved:
-            return
-        if target.isdigit():
-            solved[target] = int(target)
-            return
-        if len(connections[target]) == 1:
-            find(connections[target][0])
-            solved[target] = solved[connections[target][0]]
-            return
-        op, *args = connections[target]
-        find(args[0])
-        if op == '~':
-            solved[target] = ~solved[args[0]]
-            return
-        find(args[1])
-        solved[target] = eval(f'{solved[args[0]]}{op}{solved[args[1]]}')
-    
-    find('a')
+    connections, solved = parse_inp(inp)
+    find('a', solved, connections)
     return solved['a']
         
 
 def part_two(inp):
+    connections, solved = parse_inp(inp)
+    
+    tmp = solved.copy()
+    find('a', solved, connections)
+    tmp['b'] = solved['a']
+    solved = tmp
+    find('a', solved, connections)
+    return solved['a']
+
+
+def parse_inp(inp):
     connections = {}
     solved = {}
     for line in inp:
@@ -71,31 +40,27 @@ def part_two(inp):
                 solved[tar] = int(cur)
             else:
                 connections[tar] = [cur]
-    
-    def find(target):
-        if target in solved:
-            return
-        if target.isdigit():
-            solved[target] = int(target)
-            return
-        if len(connections[target]) == 1:
-            find(connections[target][0])
-            solved[target] = solved[connections[target][0]]
-            return
-        op, *args = connections[target]
-        find(args[0])
-        if op == '~':
-            solved[target] = ~solved[args[0]]
-            return
-        find(args[1])
-        solved[target] = eval(f'{solved[args[0]]}{op}{solved[args[1]]}')
-    
-    tmp = solved.copy()
-    find('a')
-    tmp['b'] = solved['a']
-    solved = tmp
-    find('a')
-    return solved['a']
+                
+    return connections, solved
+
+
+def find(target, solved, connections):
+    if target in solved:
+        return
+    if target.isdigit():
+        solved[target] = int(target)
+        return
+    if len(connections[target]) == 1:
+        find(connections[target][0], solved, connections)
+        solved[target] = solved[connections[target][0]]
+        return
+    op, *args = connections[target]
+    find(args[0], solved, connections)
+    if op == '~':
+        solved[target] = ~solved[args[0]]
+        return
+    find(args[1], solved, connections)
+    solved[target] = eval(f'{solved[args[0]]}{op}{solved[args[1]]}')
 
 
 def main():
