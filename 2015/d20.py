7a4fb1f97e37a60bd1a9fb610f6b8579d3057e02
OpportunV <Vadya.95@mail.ru>
2020-01-21 19:16:40 +0300

    day 20

---
+++ 2015/d20.py
@@ -0,0 +1,32 @@
+def part_one(inp):
+    for target in range(inp // 100, inp, 1):
+        numbers = set()
+        for i in range(1, int(target ** .5)):
+            if target % i == 0:
+                numbers.add(i)
+                numbers.add(target // i)
+        if sum(numbers) * 10 >= inp:
+            print(numbers)
+            return target
+
+
+def part_two(inp):
+    for target in range(inp // 100, inp, 1):
+        numbers = set()
+        for i in range(1, 51):
+            if target % i == 0:
+                numbers.add(target // i)
+        if sum(numbers) * 11 >= inp:
+            # print(numbers)
+            return target
+
+
+def main():
+    inp = 29000000
+    
+    print(part_one(inp))
+    print(part_two(inp))
+    
+    
+if __name__ == '__main__':
+    main()
